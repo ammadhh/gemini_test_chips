6d064e13990ae753a3e39e6d8a62b03f7ff35314
Ammad Hassan <ammadhassan@Ammads-Macbook.local>
2025-07-02 23:26:43 -0500

    feat: Add card flip animation

--- PokerApp/CardView.swift
+++ PokerApp/CardView.swift
@@ -5,14 +5,25 @@ struct CardView: View {
     let card: Card
     var isFaceUp: Bool
     
+    @State private var flipped: Bool = false
+
     var body: some View {
-        Image(isFaceUp ? card.imageName : "card_back")
+        Image(flipped ? card.imageName : "card_back")
             .resizable()
             .aspectRatio(contentMode: .fit)
             .frame(width: 50, height: 70)
             .background(Color.white)
             .cornerRadius(5)
             .shadow(radius: 2)
+            .rotation3DEffect(Angle.degrees(flipped ? 0 : 180), axis: (x: 0, y: 1, z: 0))
+            .onAppear {
+                flipped = isFaceUp
+            }
+            .onChange(of: isFaceUp) { newValue in
+                withAnimation(.linear(duration: 0.3)) {
+                    flipped = newValue
+                }
+            }
     }
 }
 
